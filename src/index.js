655ee0a408d206364cb6f90b4b1e448db18a8998
Julie C <jucheng925@gmail.com>
2023-04-26 11:05:33 -0400

    building the toy app

--- src/index.js
+++ src/index.js
@@ -12,4 +12,74 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  loadToy()
 });
+
+function loadToy() {
+  fetch("http://localhost:3000/toys")
+  .then(resp=>resp.json())
+  .then(data=> data.forEach(toy => renderOneToy(toy)))
+}
+
+function renderOneToy(toy){
+  let card = document.createElement("div")
+  card.className = "card"
+  card.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>
+  `
+  card.querySelector('.like-btn').addEventListener("click",()=> {
+      toy.likes++
+      card.querySelector('p').textContent = `${toy.likes} Likes`
+      updateLikes(toy)
+  })
+  document.querySelector("#toy-collection").appendChild(card)
+}
+
+
+const createButton = document.querySelector(".add-toy-form")
+createButton.addEventListener("submit", handleForm)
+
+function handleForm(e) {
+  e.preventDefault()
+  let toyObj = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+  renderOneToy(toyObj)
+  addNewToy(toyObj)
+}
+
+
+function addNewToy(toyObj){
+  fetch("http://localhost:3000/toys", {
+  method: "POST",
+  headers:
+  {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  body: JSON.stringify(toyObj)
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+
+function updateLikes(toyObj){
+  fetch(`http://localhost:3000/toys/${toyObj.id}`, {
+    method: "PATCH",
+    headers:
+    {
+     "Content-Type": "application/json",
+     Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": toyObj.likes
+    })
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
